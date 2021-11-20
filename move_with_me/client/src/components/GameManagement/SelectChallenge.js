811c223621fab98c8f27ac8173445e9c72bdb06a
Belle Sim <bellesimbeier@gmail.com>
2021-11-20 21:40:39 +0800

    Added dynamic select challenges buttons

--- move_with_me/client/src/components/GameManagement/SelectChallenge.js
+++ move_with_me/client/src/components/GameManagement/SelectChallenge.js
@@ -1,47 +1,51 @@
 import React, { useState } from "react";
 
-const data = [
-  {
-    _uid: "1",
-    component: "challenge",
-    headline: "Challenge"
-  },
-  {
-    _uid: "2",
-    component: "challenge",
-    title: "Challenge"
-  },
+// const data = [
+//   {
+//     _uid: "1",
+//     component: "challenge",
+//     headline: "Challenge"
+//   },
+//   {
+//     _uid: "2",
+//     component: "challenge",
+//     title: "Challenge"
+//   },
 
-]
+// ]
 
-function NumberList(props) {
-  const challenges = props.challenges;
-  const listItems = challenges.map((challenge) =>
+// function NumberList(props) {
+//   const challenges = props.challenges;
+//   const listItems = challenges.map((challenge) =>
 
-    <button key={challenge._uid} class="p-2 pl-5 pr-5 bg-blue-500 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300">{challenge._uid}</button>
+//     <button key={challenge._uid} class="p-2 pl-5 pr-5 bg-blue-500 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300">{challenge._uid}</button>
 
-  );
-  return (
-    <div>{listItems}</div>
-  );
-}
+//   );
+//   return (
+//     <div>{listItems}</div>
+//   );
+// }
 
 const numbers = [1, 2, 3, 4, 5];
 
+
 const SelectChallenge = () => {
   return (
-
-    <div class="flex h-screen">
-      <div class="m-auto">
-        <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
-          <div>
-            <h2 class="text-gray-800 text-3xl font-semibold">Select Your Challenge</h2>
-            <NumberList challenges={data}/>
-          </div>
-        </div>
+    <div class="text-center">
+      <h2 class="text-4xl  font-semibold  pt-24">Select Your Challenge</h2>
+      {/* <NumberList challenges={data}/> */}
+      <div class="space-x-32 pt-12">
+        {numbers.map((numbers) => (
+          <Button
+            type="primary"
+            style={{ fontSize: "25px", width: "3em", height: "3em" }}
+          >
+            {numbers}
+          </Button>
+        ))}
       </div>
     </div>
-
   );
 };
 export default SelectChallenge;
+
