d90f1f02cc97e09421efe9845ebb340c39b73454
Belle Sim <bellesimbeier@gmail.com>
2021-11-20 21:35:03 +0800

    Added dynamic select challenges buttons

--- move_with_me/client/src/components/GameManagement/SelectChallenge.js
+++ move_with_me/client/src/components/GameManagement/SelectChallenge.js
@@ -1,6 +1,21 @@
 import React, { useState } from "react";
 
 const SelectChallenge = () => {
-  return <div>This is select challenge</div>;
+  return (
+    <div class="text-center">
+      <h2 class="text-4xl  font-semibold  pt-24">Select Your Challenge</h2>
+      {/* <NumberList challenges={data}/> */}
+      <div class="space-x-32 pt-12">
+        {numbers.map((numbers) => (
+          <Button
+            type="primary"
+            style={{ fontSize: "25px", width: "3em", height: "3em" }}
+          >
+            {numbers}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default SelectChallenge;
