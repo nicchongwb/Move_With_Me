f52f6565561af4766cd3c45f1854a72da28dd917
Belle Sim <bellesimbeier@gmail.com>
2021-11-26 20:30:33 +0800

    created command trays

--- move_with_me/client/src/components/GameManagement/Challenges/Challenge.js
+++ move_with_me/client/src/components/GameManagement/Challenges/Challenge.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
+
 import "../../../assets/css/startGame.css";
 import {
   ArrowDownOutlined,
@@ -6,119 +7,88 @@ import {
   ArrowLeftOutlined,
   ArrowRightOutlined,
 } from "@ant-design/icons";
+import { Card } from "antd";
 
-class Challenge extends Component {
-  state = {
-    tasks: [
-      { name: "Up", arrow: <ArrowUpOutlined />, category: "wip" },
-      { name: "Down", arrow: <ArrowDownOutlined />, category: "wip" },
-      { name: "Left", arrow: <ArrowLeftOutlined />, category: "complete" },
-      { name: "Right", arrow: <ArrowRightOutlined />, category: "complete" },
-    ],
+const Challenge = () => {
+  //dragging item
+  const draggingItem = useRef();
+  //switch arrow position
+  const dragOverItem = useRef();
+  //list of arrows stored in state
+  const [list, setList] = useState([
+    <ArrowUpOutlined />,
+    <ArrowDownOutlined />,
+    <ArrowLeftOutlined />,
+    <ArrowDownOutlined />,
+  ]);
+
+  const handleDragStart = (e, position) => {
+    //find current position of item
+    draggingItem.current = position;
+  };
+  const handleDragEnter = (e, position) => {
+    dragOverItem.current = position;
+  };
+  const handleDragEnd = (e) => {
+    const listCopy = [...list];
+    const draggingItemContent = listCopy[draggingItem.current];
+    listCopy.splice(draggingItem.current, 1);
+    listCopy.splice(dragOverItem.current, 0, draggingItemContent);
+
+    draggingItem.current = null;
+    dragOverItem.current = null;
+    //set the new position of arrows
+    setList(listCopy);
   };
 
-  onDragStart = (ev, id) => {
-    console.log("dragstart:", id);
-    ev.dataTransfer.setData("id", id);
-  };
+  const grids = [0, 1, 2, 4, 5];
 
-  onDragOver = (ev) => {
-    ev.preventDefault();
-  };
-
-  onDrop = (ev, cat) => {
-    let id = ev.dataTransfer.getData("id");
-
-    let tasks = this.state.tasks.filter((task) => {
-      if (task.name == id) {
-        task.category = cat;
-      }
-      return task;
-    });
-
-    this.setState({
-      ...this.state,
-      tasks,
-    });
-  };
-
-  render() {
-    var tasks = {
-      wip: [],
-      complete: [],
-    };
-
-    this.state.tasks.forEach((t) => {
-      tasks[t.category].push(
-        <div
-          key={t.name}
-          onDragStart={(e) => this.onDragStart(e, t.name)}
-          draggable
-        >
-          {t.arrow}
-        </div>
-      );
-    });
-
-    return (
-      <div class=" background w-full min-h-screen opacity-80 text-center ">
-        <div class="pt-20 text-center">
-          <h1 className="text-5xl text-blue-800 mt-12 text-center font-bold  ">
-            Move With Me
-          </h1>
-        </div>
-
-        <div class="float-right text-right mr-8">
-          <p class="text-xl">Hello Belle! </p>
-          <p class=" text-green-700"> Connection Status</p>
-        </div>
-
-        <div class="flex mt-20 justify-evenly">
-          <div class="">
-            <h2 class="text-xl">Your Instruction </h2>
-            <div class="h-4/5 w-full bg-gray-200 text-5xl ">
-              <p>
-                <ArrowUpOutlined></ArrowUpOutlined>
-                <ArrowDownOutlined></ArrowDownOutlined>
-                <ArrowLeftOutlined></ArrowLeftOutlined>
-              </p>
-              <p>
-                <ArrowLeftOutlined></ArrowLeftOutlined>
-                <ArrowUpOutlined></ArrowUpOutlined>
-                <ArrowDownOutlined></ArrowDownOutlined>
-              </p>
-              <p>
-                <ArrowUpOutlined></ArrowUpOutlined>
-                <ArrowLeftOutlined></ArrowLeftOutlined>
-                <ArrowLeftOutlined></ArrowLeftOutlined>
-              </p>
-            </div>
-          </div>
-          <div>
-            <h2 class="text-xl">Controls </h2>
-            <div
-              class="h-4/5 w-full bg-gray-200 text-5xl "
-              onDragOver={(e) => this.onDragOver(e)}
-              onDrop={(e) => {
-                this.onDrop(e, "wip");
-              }}
-            >
-              <div class="text-5xl"> {tasks.wip}</div>
-            </div>
-          </div>
-          <div>
-            <h2 class="text-xl">Drop Car Commands </h2>
-            <div
-              class="h-4/5 w-full bg-gray-200 text-5xl "
-              onDragOver={(e) => this.onDragOver(e)}
-              onDrop={(e) => this.onDrop(e, "complete")}
-            >
-              <div class="text-5xl"> {tasks.complete}</div>
-            </div>
-          </div>
-        </div>
+  return (
+    <div class=" background w-full min-h-screen opacity-80 text-center ">
+      <div class="pt-20 text-center">
+        <h1 className="text-5xl text-blue-800 mt-12 text-center font-bold  ">
+          Move With Me
+        </h1>
       </div>
-    );
-  }
-}
+      <div class="float-right text-right mr-8">
+        <p class="text-xl">Hello Belle! </p>
+        <p class=" text-green-700"> Connection Status</p>
+      </div>
+      <div class="flex mt-20 justify-center">
+        <Card title="Navigate Map" style={{ width: 500 }}>
+          <div class="flex">
+            {grids.map(() => (
+              <div class="w-24 h-24 bg-gray-300 border-black  border-4"></div>
+            ))}
+          </div>
+          <div class="flex">
+            {grids.map(() => (
+              <div class="w-24 h-24 bg-gray-300 border-black  border-4"></div>
+            ))}
+          </div>
+        </Card>
+
+        <Card title="Controls" style={{ width: 500 }}>
+          {list &&
+            list.map((item, index) => (
+              <div
+                onDragStart={(e) => handleDragStart(e, index)}
+                onDragEnd={handleDragEnd}
+                key={index}
+                draggable
+                class="text-5xl"
+              >
+                {item}
+              </div>
+            ))}
+        </Card>
+      </div>
+      <div class="flex justify-center">
+        <Card title="Command Tray" style={{ width: 1000 }}>
+          <div></div>
+        </Card>
+      </div>
+    </div>
+  );
+};
 export default Challenge;
