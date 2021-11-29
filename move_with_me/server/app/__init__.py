9d2f39f0f6ccf7ece21bceca1e54aba891cca081
Z-Jian <2002689@sit.singaporetech.edu.sg>
2021-11-29 15:52:52 +0800

    Created mongo altas connection, read from database collectionand make it dynamic, passing props to challenge component wip

--- move_with_me/server/app/__init__.py
+++ move_with_me/server/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymongo
 from pymongo import MongoClient
 
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mvm_db"
+CORS(app)
+app.config["MONGO_URI"] = "mongodb+srv://admin:ICT2x01isgreat@cluster0.ciqn1.mongodb.net/mvm_db?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 db = mongo.db
 
@@ -22,6 +24,6 @@ def retrieve_challenge():
     for data in x:
         data['_id'] = str(data['_id']) # This does the trick!
         challenges.append(data)
-        return jsonify(challenges)
+    return jsonify(challenges)
 
 from app.routes import home, users, react_test
