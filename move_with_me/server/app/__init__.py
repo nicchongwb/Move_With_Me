0ac23324bcd8a2fa4ba61f5be6e72d110eb3a675
khoirun96 <khoirunilman@gmail.com>
2021-12-01 15:51:19 +0800

    add files before pull dev

--- move_with_me/server/app/__init__.py
+++ move_with_me/server/app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 import pymongo
+import os
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mvm_db"
@@ -14,5 +19,20 @@ def helloHandler():
     if request.method == 'GET':
         return 'Hello EcSP8266'
 
+
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')  # getting JWT_SECRET key from .env file
+jwt = JWTManager(app)
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/token", methods=["POST"])
+def tokenCreation():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
 
 from app.routes import home, users, react_test
---
+++ move_with_me/server/app/routes/app.py
@@ -0,0 +1,8 @@
+# from flask import Flask, request
+# from flask_jwt_extended import JWTManager
+
+# app = Flask(__name__)
+
+
+# app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+# jwt = JWTManager(app)
