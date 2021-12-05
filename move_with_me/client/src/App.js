dac9c5c04a2980c886252168994cd264f3daee94
bellesim <bellesimbeier@gmail.com>
2021-12-05 16:51:19 +0800

    pulling changes

--- move_with_me/client/src/App.js
+++ move_with_me/client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StartGame from "./components/GameManagement/StartGame";
@@ -106,96 +105,10 @@ function App() {
           exact
           component={GhLogin}
         ></Route>
-        <Route
-          exact
-          key="login"
-          path="/login"
-          exact
-          component={login}
-        ></Route>
+        <Route exact key="login" path="/login" exact component={login}></Route>
       </Router>
     </div>
   );
 }
 
 export default App;
-=======
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import StartGame from "./components/GameManagement/StartGame";
-import SetPlayerName from "./components/GameManagement/SetPlayerName";
-import DoTutorial from "./components/GameManagement/DoTutorial";
-import SelectChallenge from "./components/GameManagement/SelectChallenge";
-import Map from "./components/GameManagement/Map";
-import Challenge from "./components/GameManagement/Challenge";
-import CarSpecs from "./components/GameManagement/CarSpecs";
-import Ranking from "./components/GameManagement/Ranking";
-import CreateMap from "./components/GameManagement/Map/CreateMap";
-
-import "tailwindcss/tailwind.css";
-import "antd/dist/antd.css";
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Route
-          exact
-          key="startGame"
-          path="/"
-          exact
-          component={StartGame}
-        ></Route>
-        <Route
-          exact
-          key="carSpecs"
-          path="/carSpecifications"
-          exact
-          component={CarSpecs}
-        ></Route>
-        <Route
-          exact
-          key="doTutorial"
-          path="/tutorial"
-          exact
-          component={DoTutorial}
-        ></Route>
-
-        <Route
-          exact
-          key="setPlayerName"
-          path="/setPlayerName"
-          exact
-          component={SetPlayerName}
-        ></Route>
-
-        <Route
-          exact
-          key="selectChallenge"
-          path="/selectChallenge"
-          exact
-          component={SelectChallenge}
-        ></Route>
-        <Route exact key="map" path="/map" exact component={Map}></Route>
-        <Route
-          exact
-          key="challenge"
-          path="/challenge"
-          exact
-          component={Challenge}
-        ></Route>
-        <Route
-          exact
-          key="ranking"
-          path="/ranking"
-          exact
-          component={Ranking}
-        ></Route>
-        <Route exact key="createMap" path="/createMap" exact component={CreateMap}></Route>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 82139770f021ee763bd1b3f4db9a57728c717e6b
