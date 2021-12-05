4ccc670ceea6d53bdb87d29240849ba04d86e7e8
nicchongwb <nicholaschongwb@gmail.com@users.noreply.github.com>
2021-12-05 23:35:06 +0800

    Main Ranking fetch data is complete

--- move_with_me/client/src/components/GameManagement/Ranking.js
+++ move_with_me/client/src/components/GameManagement/Ranking.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../assets/css/startGame.css";
 import { Link } from "react-router-dom";
 import { Card, Button, Table } from "antd";
+import axios from "axios";
 
 const Ranking = () => {
+  const [recordArr, setRecordArr] = useState([]); // State to store our ranking from mongo -> flask -> react
+
+  const fetchRanking = () => {
+    const headers = {
+      "Access-Control-Allow-Origin": "http://localhost:5000",
+    }
+
+    // AXIOS to send GET request to /api/rankings
+    return axios.post('/api/rankings', headers)
+    .then(function(response){
+      setRecordArr(response.data.rankingData)
+      console.log(response.data.rankingData)      
+    });
+  };
+
+  // EffectHook to fetchRanking upon first render
+  useEffect(() => {
+    console.log("fetching rankings...")
+    fetchRanking()
+  },[])
+
   const columns = [
     {
       title: "Player Name",
@@ -11,12 +33,7 @@ const Ranking = () => {
       key: "playerName",
     },
     {
-      title: "Challenge Name",
-      dataIndex: "challenge",
-      key: "challenge",
-    },
-    {
-      title: "Score",
+      title: "Total Score",
       dataIndex: "score",
       key: "score",
     },
@@ -26,7 +43,7 @@ const Ranking = () => {
       <div class="pt-72 text-center">
         <h1 class="font-semibold text-8xl text-gray-800">Ranking</h1>
         <div class="w-4/6 m-auto pt-24">
-          <Table columns={columns} />
+          <Table dataSource={recordArr} columns={columns} />
         </div>
 
         <div class="pt-12">
